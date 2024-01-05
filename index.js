df1e51699fc33972d5daae1cb3c5f605b099e97d
FaithOgendi <faithbrianna01@gmail.com>
2024-01-05 11:13:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',()=>{
+    let m=document.querySelector('#text')
+    m.textContent='This is really cool!'
+})
+
+let m=document.querySelector('m')
+p.textContent='JavaScript is so cool. It lets me add text to my page programmatically.'
